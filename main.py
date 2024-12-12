8dc66326b208edcd6e87439c0d738daffdb8572d
JiaruiZhang <zhangjiarui_0209@163.com>
2024-12-11 19:24:38 -0500

    commit test 2

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print("Finall I am all set to push anything to github!")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
